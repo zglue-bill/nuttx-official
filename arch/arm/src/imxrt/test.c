8966699fff7b8d1d31288d3d5e9a6cf6a05adfb9
Bill <bill@zglue.com>
2019-02-27 02:37:12 -0800

    File ended up restyled, fix.

--- arch/arm/src/imxrt/test.c
+++ arch/arm/src/imxrt/test.c
@@ -1,6 +1,6 @@
-
-#define IMXRT_ITCM_BASE           0x00000000  /* 512KB ITCM */
-/* 0x00080000     512KB ITCM Reserved */
-/* 0x00100000     1MB ITCM Reserved */
-#define IMXRT_ROMCP_BASE          0x00200000  /* 96KB ROMCP */
-/* 0x00218000     416KB ROMCP Reserved */
+                                /* 0x00080000     512KB ITCM Reserved */
+#define IMXRT_ITCM_BASE         0x00000000  /* 512KB ITCM */
+                                /* 0x00080000     512KB ITCM Reserved */
+                                /* 0x00100000     1MB ITCM Reserved */
+#define IMXRT_ROMCP_BASE        0x00200000  /* 96KB ROMCP */
+                                /* 0x00218000     416KB ROMCP Reserved */
