7985f02a384a409c5b493bf034a745b33ffdf988
Bill <bill@zglue.com>
2019-02-28 01:07:10 -0800

    Change duplicate address for easier debugging.

--- arch/arm/src/imxrt/test.c
+++ arch/arm/src/imxrt/test.c
@@ -1,4 +1,4 @@
-                                /* 0x00080000     512KB ITCM Reserved */
+                                /* 0x000C0000     512KB ITCM Reserved */
 #define IMXRT_ITCM_BASE         0x00000000  /* 512KB ITCM */
                                 /* 0x00080000     512KB ITCM Reserved */
                                 /* 0x00100000     1MB ITCM Reserved */
