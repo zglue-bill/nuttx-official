ad05263e18993a330e2bf0df9f0f9cb79aeb3bbc
Bill <bill@zglue.com>
2019-03-06 19:57:21 -0800

    Save off newest changes that cause astyle to hang.
    Change-Id: I57ad7ae4bf4f2b29cf83a824343bcd080397170a

--- arch/arm/src/imxrt/test.c
+++ arch/arm/src/imxrt/test.c
@@ -1,7 +1,41 @@
                                 /* 0x000C0000     512KB ITCM Reserved */
 #define IMXRT_ITCM_BASE         0x00000000  /* 512KB ITCM */
                                 /* 0x00080000     512KB ITCM Reserved */
-
                                 /* 0x00100000     1MB ITCM Reserved */
 #define IMXRT_ROMCP_BASE        0x00200000  /* 96KB ROMCP */
                                 /* 0x00218000     416KB ROMCP Reserved */
+
+        /* This controller */
+
+struct imxrt_edma_s
+{
+  /* These tables */
+
+  sem_t chsem;                    /* channel table */
+#if CONFIG > 0
+  sem_t dsem;                     /* free descriptors */
+#endif
+}
+                           /* Align here? */
+void imxrt(void)
+{
+  int ret;
+
+  do
+    {
+      /* Take the ) */
+
+      ret = nxsem_wait(&g_edma.chsem);
+
+      /* The only
+       * awakened
+       */
+    }
+  while (ret == -EINTR);
+}
+
+void em(void)
+{
+  (void)nxse(&g_edma.chsem);
+}
+
