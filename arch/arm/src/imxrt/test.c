3469cd42e7d83df48d1afc806f96ceaa65016366
Bill <bill@zglue.com>
2019-03-15 14:33:34 -0700

    Add functions and embedded comments
    Change-Id: If4c2ccf236b2ff582bf0f4e4adda251bf5797ba5

--- arch/arm/src/imxrt/test.c
+++ arch/arm/src/imxrt/test.c
@@ -3,7 +3,22 @@
                                 /* 0x001C0000     512KB ITCM Reserved */
 #define IMXRT_ITCM_BASE         0x001C0000  /* 512KB ITCM */
                                 /* 0x00080000     512KB ITCM Reserved */
+;                               /* 0x00180000     512KB ITCM Reserved */
+.                               /* 0x00280000     512KB ITCM Reserved */
+//                              /* 0x00380000     512KB ITCM Reserved */
 
                                 /* 0x00100000     1MB ITCM Reserved */
 #define IMXRT_ROMCP_BASE        0x00200000  /* 96KB ROMCP */
                                 /* 0x00218000     416KB ROMCP Reserved */
+
+
+int
+main(int argc, char *argv)
+{
+        int foo;
+
+        /* 0x00FF0000     lksajdflajslkdfjved */
+        foo = 1;
+        printf("foo is %d\n", foo);
+}
+
